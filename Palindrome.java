6dd886d4c7f4048d4333f266d2915a851acd5846
padma464 <114167689+padma464@users.noreply.github.com>
2022-09-24 23:41:34 +0530

    import java.util.Scanner; class palindrome {     public static boolean ispalindrome(int m)     {         int sum=0,r,temp=m;         while(m>0)         {             r=m%10;             sum=sum*10+r;             m=m/10;         }         if(sum==temp)            return true;         else            return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,res;         n=sc.nextInt();         n=Math.abs(n);         if(ispalindrome(n))          System.out.println("True");         else           System.out.println("False");
        }
    }

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class palindrome
+{
+    public static boolean ispalindrome(int m)
+    {
+        int sum=0,r,temp=m;
+        while(m>0)
+        {
+            r=m%10;
+            sum=sum*10+r;
+            m=m/10;
+        }
+        if(sum==temp)
+           return true;
+        else
+           return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,res;
+        n=sc.nextInt();
+        n=Math.abs(n);
+        if(ispalindrome(n))
+         System.out.println("True");
+        else
+          System.out.println("False");
+        
+    }
+}
