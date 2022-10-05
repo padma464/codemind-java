463c3ede3a9c6fe8865c8fa9c260e7aba59e1396
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-05 14:22:34 +0530

    import java.util.Scanner; class fib {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a=0,b=1,c;         n=sc.nextInt();         c=a+b;         System.out.print(a+" "+b+" ");         for(int i=3;i<=n;i++)         {             c=a+b;             System.out.print(c+" ");             a=b;             b=c;         }     } }

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-class fibonacci
+class fib
 {
     public static void main(String args[])
     {
-        int a=0,b=1,c,n;
         Scanner sc=new Scanner(System.in);
+        int n,a=0,b=1,c;
         n=sc.nextInt();
         c=a+b;
         System.out.print(a+" "+b+" ");
-        for(int i=1;i<n-1;i++)
+        for(int i=3;i<=n;i++)
         {
+            c=a+b;
             System.out.print(c+" ");
             a=b;
             b=c;
-            c=a+b;
-            
         }
     }
 }
