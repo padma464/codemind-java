f59814fd27447c118ea4e66b1cc37647cdfad9ed
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-05 14:52:13 +0530

    import java.util.Scanner; class line {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,s=0;         n=sc.nextInt();         for(int i=1;i<=(int)(n/2);i++)         {            if(n%i==0)            {                s+=i;            }         }         if(s>n)           System.out.println("True");         else           System.out.println("False");     } }

--- Abundant_Numbers.java
+++ Abundant_Numbers.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-class nmbr
+class line
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n,s=0;
         n=sc.nextInt();
-        for(int i=1;i<=n/2;i++)
+        for(int i=1;i<=(int)(n/2);i++)
         {
-            if(n%i==0)
-             s+=i;
+           if(n%i==0) 
+           {
+               s+=i;
+           }
         }
         if(s>n)
           System.out.println("True");
         else
-           System.out.println("False");
+          System.out.println("False");
     }
 }
