8068363fec1b27565ad132bb642cfd0fdd68de8b
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-07 20:02:11 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,i,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         a=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]==a)             {               c++;             }         }         System.out.println(c);     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],a,i,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+        a=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]==a)
+            {
+              c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
