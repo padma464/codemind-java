b78fef7d0ab091b2a6cfbf3108ed7dce44f79294
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-07 22:47:55 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0,s=0;         float d;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n;i++)         {           s+=x[i];         }         d=(int)s/n;         for(i=0;i<n;i++)         {             if(x[i]>=d)               count++;         }         System.out.println(count);     } }

--- elements_greater_than_average.java
+++ elements_greater_than_average.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-class array
+class diff
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,s=0,c=0;
-        float a;
+        int n,x[],i,count=0,s=0;
+        float d;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-         x[i]=sc.nextInt();
+           x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-           s+=x[i];
+          s+=x[i]; 
         }
-        a=s/n;
+        d=(int)s/n;
         for(i=0;i<n;i++)
         {
-            if(x[i]>=a)
-              c++;
+            if(x[i]>=d)
+              count++;
         }
-        System.out.println(c);
-        
+        System.out.println(count);
     }
 }
