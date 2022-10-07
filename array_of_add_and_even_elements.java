1533a9fcb86caa49c88e504677377c2849656330
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-07 21:39:02 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n;i++)         {            if(x[i]%2!=0)              System.out.print(x[i]+" ");         }         for(i=0;i<n;i++)         {            if(x[i]%2==0)              System.out.print(x[i]+" ");         }     } }

--- array_of_add_and_even_elements.java
+++ array_of_add_and_even_elements.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class array
+class diff
 {
     public static void main(String args[])
     {
@@ -8,16 +8,16 @@ class array
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-         x[i]=sc.nextInt();
+           x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            if(x[i]%2!=0)
-              System.out.print(x[i]+" ");
+           if(x[i]%2!=0)
+             System.out.print(x[i]+" ");
         }
         for(i=0;i<n;i++)
         {
-            if(x[i]%2==0)
-              System.out.print(x[i]+" ");
+           if(x[i]%2==0)
+             System.out.print(x[i]+" ");
         }
     }
 }
