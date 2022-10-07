22d506da83f30ef7b5d31b08f48e349ed41387bd
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-07 13:23:10 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],s=0,i,m;         n=sc.nextInt();         m=n;         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();             if(x[i]%2!=0)             {                s=i;             }         }             System.out.println(s);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],s=0,i,m;
+        n=sc.nextInt();
+        m=n;
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+            if(x[i]%2!=0)
+            {
+               s=i;
+            }
+        }
+            System.out.println(s);
+    }
+}
