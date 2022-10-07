370c2cbdb5e1cc203cfbaa4e70559410d0341acd
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-07 21:16:40 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n-2;i++)         {            if((x[i]%2!=0)&&(x[i+2]%2!=0))               if(x[i+1]%2==0)               count++;         }         System.out.println(count);
        }
    }

---
+++ count_even_numbers_between_odd_numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+        for(i=0;i<n-2;i++)
+        {
+           if((x[i]%2!=0)&&(x[i+2]%2!=0))
+              if(x[i+1]%2==0)
+              count++;
+        }
+        System.out.println(count);
+        
+    }
+}
