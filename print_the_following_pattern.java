cac0032852bef0fc23a7344f51eefde3e60e3305
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-10 20:12:04 +0530

    import java.util.Scanner;  class Pattern4 {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();
             for (int i = 1; i <=n; i++)  
            {  
                  for (int j = 1; j<= n - i; j++)   
                  {   
                           System.out.print(" ");   
                  }   
                  for (int k = i-1; k >= 0; k--)  
                   {  
                              System.out.print(k);  
                   }  
                  for (int l = 1; l <= i-1; l++)   
                   {  
                             System.out.print(l);   
                    }   
              System.out.println();   
             }   
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
  class Pattern4  
 {              
     public static void main(String[] args)   
-    { 
-        int n;
+    {  
         Scanner sc=new Scanner(System.in);
+        int n;
         n=sc.nextInt();
-        for (int i = 1; i <= n; i++)  
+        
+         for (int i = 1; i <=n; i++)  
         {  
-            for (int j = 1; j<= n - i; j++)   
-            {   
-                System.out.print(" ");   
-            }   
-            for (int k = i; k >= 1; k--)  
-            {  
-                System.out.print(i);  
-            }  
-            for (int l = 2; l <= i; l++)   
-            {  
-                System.out.print(i);   
-            }   
-            
+              for (int j = 1; j<= n - i; j++)   
+              {   
+                       System.out.print(" ");   
+              }   
+              for (int k = i-1; k >= 0; k--)  
+               {  
+                          System.out.print(k);  
+               }  
+              for (int l = 1; l <= i-1; l++)   
+               {  
+                         System.out.print(l);   
+                }   
           System.out.println();   
-        } 
+         }   
     }
 }
