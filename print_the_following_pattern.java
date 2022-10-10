080d4df80b90e372ca8f8a5534a0bb967d40a440
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-10 18:07:17 +0530

    import java.util.Scanner;  class Pattern4 {     public static void main(String[] args)     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for (int i = 1; i <= n; i++)         {             for (int j = 1; j<= n - i; j++)             {                 System.out.print(" ");             }             for (int k = i; k >= 1; k--)             {                 System.out.print(i);             }             for (int l = 2; l <= i; l++)             {                 System.out.print(i);             }
              System.out.println();   
            } 
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
-class sample
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
+ class Pattern4  
+{              
+    public static void main(String[] args)   
+    { 
         int n;
+        Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-            for(int i=1;i<=n;i++)
-            {
-                for(int j=1;j<=i;j++)
-                {
-                     System.out.print(j);
-                }
-                System.out.println();
-            }
+        for (int i = 1; i <= n; i++)  
+        {  
+            for (int j = 1; j<= n - i; j++)   
+            {   
+                System.out.print(" ");   
+            }   
+            for (int k = i; k >= 1; k--)  
+            {  
+                System.out.print(i);  
+            }  
+            for (int l = 2; l <= i; l++)   
+            {  
+                System.out.print(i);   
+            }   
+            
+          System.out.println();   
+        } 
     }
 }
