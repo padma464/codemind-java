578913aa3c0681660bcddffd3627b2fefdeff008
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-14 23:34:31 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c,m,t, r;         t=sc.nextInt();         for(i=0;i<t;i++)         {             m=sc.nextInt();             r=(int)Math.sqrt(m);             c=r*r;             if(c==m)               System.out.println("True");             else               System.out.println("False");         }     } }

---
+++ Valid_Perfect_Square.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c,m,t, r;
+        t=sc.nextInt();
+        for(i=0;i<t;i++)
+        {
+            m=sc.nextInt();
+            r=(int)Math.sqrt(m);
+            c=r*r;
+            if(c==m)
+              System.out.println("True");
+            else
+              System.out.println("False");
+        }
+    }
+}
