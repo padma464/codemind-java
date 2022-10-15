b9f2c1b246f47191af662e977190393d661259c2
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 15:35:25 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,c=0,m;         n=sc.nextInt();         m=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]%m!=0)               c++;         }          System.out.println(c);     } }

--- Count_elements_not_divisible_by_K.java
+++ Count_elements_not_divisible_by_K.java
@@ -4,18 +4,17 @@ class diff
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],a,i,c=0;
+        int n,x[],i,c=0,m;
         n=sc.nextInt();
-        a=sc.nextInt();
+        m=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-           x[i]=sc.nextInt();
+            x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-          if(x[i]%a!=0)
-            c++;
+            if(x[i]%m!=0)
+              c++;
         }
-        
-           System.out.println(c);
+         System.out.println(c);
     }
 }
