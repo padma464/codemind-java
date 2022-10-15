444cda2c8d7606caf7b8ee0920fde3be7e7a30ca
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 14:10:56 +0530

    import java.util.Scanner; class array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,b,i,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {               System.out.print(x[i]+" ");               c++;             }         }         if(c==0)           System.out.println("-1");     } }

--- elements_in_between_A_and_B.java
+++ elements_in_between_A_and_B.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
-class diff
+class array
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,a,b,c=0;
+        int n,x[],a,b,i,c=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-            x[i]=sc.nextInt();
+         x[i]=sc.nextInt();
         a=sc.nextInt();
         b=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            if(x[i]>=a&&(x[i]<=b))
+            if(x[i]>=a && x[i]<=b)
             {
-               System.out.print(x[i]+" ");
-               c++;
+              System.out.print(x[i]+" ");
+              c++;
             }
         }
         if(c==0)
-           System.out.println("-1");
+          System.out.println("-1");
     }
 }
