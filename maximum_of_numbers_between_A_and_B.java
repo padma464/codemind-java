2e6f8ff1e69a3ef572668022be9564827cabae0e
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 14:21:31 +0530

    import java.util.Scanner; class array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,b,i,s,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         s=x[0];         for(i=0;i<n;i++)         {             if(s>x[i])               s=x[i];         }         for(i=0;i<n;i++)         {             if(x[i]>=a &&x[i]<=b)             {               if(s<x[i])               {                 s=x[i];               c++;               }             }         }         if(c!=0)         System.out.println(s);         else           System.out.println("-1");     } }

--- maximum_of_numbers_between_A_and_B.java
+++ maximum_of_numbers_between_A_and_B.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
-class diff
+class array
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],a,i,s=0,r=0,c=0,b;
+        int n,x[],a,b,i,s,c=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-          x[i]=sc.nextInt();
+         x[i]=sc.nextInt();
         a=sc.nextInt();
         b=sc.nextInt();
-        r=x[0];
+        s=x[0];
         for(i=0;i<n;i++)
         {
-           if((x[i]>=a) && (x[i]<=b))
-           {
-              if(r<x[i])
-              {
-                r=x[i];
-                c++;
-              }
-           }
+            if(s>x[i])
+              s=x[i];
         }
-        if(c>0)
-        System.out.println(r);
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=a &&x[i]<=b)
+            {
+              if(s<x[i])
+              {
+                s=x[i];
+              c++;
+              }
+            }
+        }
+        if(c!=0)
+        System.out.println(s);
         else
           System.out.println("-1");
     }
