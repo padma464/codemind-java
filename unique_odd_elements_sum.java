6042ae53e2651f4534e7a06c404e8a42e601e791
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 13:57:55 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],j,i,r,m=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             r=x[i];             if(x[i]!=-999)             {                for(j=0;j<n;j++)                {                    if(x[j]==x[i] && i!=j)                    {                        x[j]=-999;                    }                 }                 if(x[i]%2!=0)                    m+=x[i];             }         }           System.out.println(m);     } }

--- unique_odd_elements_sum.java
+++ unique_odd_elements_sum.java
@@ -4,25 +4,27 @@ class diff
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],s,i,sum=0;
+        int n,x[],j,i,r,m=0;
         n=sc.nextInt();
         x=new int[n];
-        for (i=0 ;i<n;i++)
-          x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            if(x[i]!=-999 &&x[i]%2!=0)
+            r=x[i];
+            if(x[i]!=-999)
             {
-                for(int j=0;j<n;j++)
-                {
-                    if(x[i]==x[j] && i!=j)
-                    {
-                        x[j]=-999;
-                    }
+               for(j=0;j<n;j++)
+               {
+                   if(x[j]==x[i] && i!=j)
+                   {
+                       x[j]=-999;
+                   }
                 }
-                sum=sum+x[i];
+                if(x[i]%2!=0)
+                   m+=x[i];
             }
         }
-        System.out.println(sum);
+          System.out.println(m);
     }
 }
