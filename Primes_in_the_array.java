b01ef8330cf68de73295ad6e02058507de843d8b
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 15:22:03 +0530

    import java.util.Scanner; class primesbtwminandmax {     public static boolean isprime(int n)     {         if(n==1)           return false;         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)           return true;         else           return false;     }     public static void main(String args[])     {         int n,x[],c=0,i,s,k;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for( i=0;i<n;i++)             x[i]=sc.nextInt();         for( i=0;i<n;i++)         {             if(isprime(x[i]))                    c++;         }         System.out.println(c);
        }
    }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class primesbtwminandmax
+{
+    public static boolean isprime(int n)
+    {
+        if(n==1)
+          return false;
+        int count=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count==0)
+          return true;
+        else
+          return false;
+    }
+    public static void main(String args[])
+    {
+        int n,x[],c=0,i,s,k;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for( i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for( i=0;i<n;i++)
+        {
+            if(isprime(x[i]))
+                   c++;
+        }
+        System.out.println(c);
+            
+    }
+}
