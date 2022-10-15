17b5a1965b248815513bd38795e568a032439c08
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 14:24:15 +0530

    import java.util.Scanner; class array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,i,s=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();         a=sc.nextInt();         for(i=0;i<n;i++)         {             s+=x[i];             if(x[i]==a)               break;         }           System.out.println(s);     } }

--- sum_of_array__elements_upto__Kth__number_in_an_array.java
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -4,20 +4,18 @@ class array
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],k,i,c=0;
+        int n,x[],a,i,s=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
          x[i]=sc.nextInt();
-        k=sc.nextInt();
-        
+        a=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            c+=x[i];
-            if(k==x[i])
-             break;
+            s+=x[i];
+            if(x[i]==a)
+              break;
         }
-        System.out.println(c);
-        
+          System.out.println(s);
     }
 }
