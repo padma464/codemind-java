dc9f04e8df8296a3f1e2cc4e31494c24a6f0cc86
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 13:45:03 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],j,i,c=0,r,m=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             r=x[i];             c++;             if(x[i]!=-999)             {                for(j=0;j<n;j++)                {                    if(x[j]==x[i] && i!=j)                    {                        c++;                        x[j]=-999;                    }                 }                 if(r%2!=0)                   m++;             }         }          System.out.println(m);     } }

---
+++ odd_unique_elements_count.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],j,i,c=0,r,m=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            r=x[i];
+            c++;
+            if(x[i]!=-999)
+            {
+               for(j=0;j<n;j++)
+               {
+                   if(x[j]==x[i] && i!=j)
+                   {
+                       c++;
+                       x[j]=-999;
+                   }
+                }
+                if(r%2!=0)
+                  m++;
+            }
+        }
+         System.out.println(m);
+    }
+}
