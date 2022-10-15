dfae54994cbb5cbda659143418b27d8d9b324b19
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-15 13:52:55 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],j,i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             System.out.print(x[i]+" ");         }         if(n%2!=0)          System.out.print("0");     } }

--- Elements_with_pairs.java
+++ Elements_with_pairs.java
@@ -4,16 +4,16 @@ class diff
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],s,i,sum=0;
+        int n,x[],j,i;
         n=sc.nextInt();
         x=new int[n];
-        for (i=0 ;i<n;i++)
-          x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            System.out.print(x[i]+" ");  
+            System.out.print(x[i]+" ");
         }
         if(n%2!=0)
-          System.out.print("0");
+         System.out.print("0");
     }
 }
