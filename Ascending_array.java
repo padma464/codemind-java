d9809a3d2e712868270dbf306149d22ab56ea204
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-27 23:35:50 +0530

    import java.util.Scanner; class prime {     public static void main(String args[])     {         int n,x[];         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)             x[i]=sc.nextInt();         int c=0;         for(int i=0;i<n-1;i++)         {             if(x[i]<x[i+1])              c=0;             else               {                   c=1;                   break;               }         }         if(c==0)           System.out.println("yes");         else          System.out.println("no");     } }

---
+++ Ascending_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class prime
+{
+    public static void main(String args[])
+    {
+        int n,x[];
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(x[i]<x[i+1])
+             c=0;
+            else
+              {
+                  c=1;
+                  break;
+              }
+        }
+        if(c==0)
+          System.out.println("yes");
+        else 
+         System.out.println("no");
+    }
+}
