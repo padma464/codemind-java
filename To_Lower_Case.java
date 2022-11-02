19bce91967ae963f99e4d1ed5f8fa9b1b255f4d4
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-11-02 22:58:36 +0530

    import java.util.*; class Palindromestr {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         System.out.println(s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Palindromestr
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(s.toLowerCase());
+    }
+}
