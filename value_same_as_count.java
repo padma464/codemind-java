d3b17da4f6591eeaf806105b1c9707dc203215ec
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-11-03 21:14:09 +0530

    import java.util.Scanner; class diff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],j,i,c=0,r,m=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             r=x[i];             if(x[i]!=-999)             {                 c++;                for(j=0;j<n;j++)                {                    if(x[j]==x[i] && i!=j)                    {                        c++;                        x[j]=-999;                    }                 }                 if(c==r)                 {                   m++;                 }                 c=0;             }         }           System.out.println(m);     } }

---
+++ value_same_as_count.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class diff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],j,i,c=0,r,m=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            r=x[i];
+            if(x[i]!=-999)
+            {
+                c++;
+               for(j=0;j<n;j++)
+               {
+                   if(x[j]==x[i] && i!=j)
+                   {
+                       c++;
+                       x[j]=-999;
+                   }
+                }
+                if(c==r)
+                {
+                  m++;
+                }
+                c=0;
+            }
+        }
+          System.out.println(m);
+    }
+}
