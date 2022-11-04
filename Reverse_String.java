b41f80bd3a8d56b82f589034285560f1aa877e7f
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-11-04 15:49:16 +0530

    import java.lang.*; import java.io.*; import java.util.*;
    class ReverseString {
        public static void main(String[] args)
        {
            Scanner sc=new Scanner(System.in);
            String s = sc.nextLine();
            char[] ch = s.toCharArray();
      
            for (int i = ch.length - 1; i >= 0; i--)
                System.out.print(ch[i]);
        }
    }

---
+++ Reverse_String.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+  
+class ReverseString {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s = sc.nextLine();
+        char[] ch = s.toCharArray();
+  
+        for (int i = ch.length - 1; i >= 0; i--)
+            System.out.print(ch[i]);
+    }
+}
