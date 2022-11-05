5c7fa7d0d1c2605ca69724522ed0f0c64f1fbd2a
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-11-05 09:08:09 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String str[]=s.split(" ");         String p="";         for(int i=str.length-1;i>=0;i--)         {             if(str[i]!=" ")             p=p+str[i]+" ";         }         System.out.println(p.trim());     } }

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,15 +1,17 @@
-import java.lang.*;
-import java.io.*;
 import java.util.*;
-  
-class ReverseString {
-    public static void main(String[] args)
+class sample
+{
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        String s = sc.nextLine();
-        char[] ch = s.toCharArray();
-  
-        for (int i = ch.length - 1; i >= 0; i--)
-            System.out.print(ch[i]);
+        String s=sc.nextLine();
+        String str[]=s.split(" ");
+        String p="";
+        for(int i=str.length-1;i>=0;i--)
+        {
+            if(str[i]!=" ")
+            p=p+str[i]+" ";
+        }
+        System.out.println(p.trim());
     }
 }
