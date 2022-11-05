39ba1cf4b584e91868de370ae053ac7d437729d8
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-11-05 10:18:08 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         int m=0;         for(int i=0;i<s.length();i++)         {
                if(Character.isDigit(s.charAt(i)))
                m=m+Character.getNumericValue(s.charAt(i));
            }
              System.out.println(m);
        }
    }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int m=0;
+        for(int i=0;i<s.length();i++)
+        {
+
+            if(Character.isDigit(s.charAt(i)))
+            m=m+Character.getNumericValue(s.charAt(i));
+        }
+          System.out.println(m);
+    }
+}
